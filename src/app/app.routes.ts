41d00daf5402434f525e1c444a2b3b84f211be5f
Eugene Murray <eugene.murray@gmail.com>
2024-08-02 12:05:43 +0100

    add new pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CssComponent } from './pages/css/css.component';
+import { ComponentsComponent } from './pages/components/components.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'css', component: CssComponent },
+    { path: 'components', component: ComponentsComponent },
+];
