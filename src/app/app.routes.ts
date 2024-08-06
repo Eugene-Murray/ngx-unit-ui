acfe8ce1deb62eb17db4ee1125752b3ef2427a6e
Eugene Murray <eugene.murray@gmail.com>
2024-08-06 20:41:38 +0100

    add responsive flex

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CssComponent } from './pages/css/css.component';
 import { ComponentsComponent } from './pages/components/components.component';
+import { LayoutsComponent } from './pages/layouts/layouts.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'css', component: CssComponent },
     { path: 'components', component: ComponentsComponent },
+    { path: 'layouts', component: LayoutsComponent },
 ];
