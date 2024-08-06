17f0e7581b3d453d9b652e63a589926f3a674967
Eugene Murray <eugene.murray@gmail.com>
2024-08-06 19:18:17 +0100

    update components

---
+++ src/app/components/more/accordion/accordion.component.ts
@@ -0,0 +1,33 @@
+import { Component, contentChildren, effect, ElementRef, viewChildren } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  standalone: true,
+  imports: [],
+  templateUrl: './accordion.component.html',
+  styleUrl: './accordion.component.scss',
+})
+export class AccordionComponent {
+  private accordions = viewChildren<ElementRef>('accordion');
+  items = contentChildren<ElementRef>('accordionItem');
+
+  constructor() {
+    effect(() => {
+      this.items().forEach((panel) => {
+        //panel.nativeElement.style.maxHeight = panel.nativeElement.scrollHeight + 'px';
+        console.warn(panel.nativeElement);
+      });
+      //console.warn(this.items().length);
+    });
+  }
+
+  click(index: number) {
+    this.accordions()[index].nativeElement.classList.toggle('active');
+    const panel = this.accordions()[index].nativeElement.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  }
+}
--- src/app/pages/components/components.component.ts
+++ src/app/pages/components/components.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SlideShowComponent } from '../../components/images/slideshow/slideshow.component';
 import { SlideShowGalleryComponent } from '../../components/images/slideshow-gallery/slideshow-gallery.component';
+import { AccordionComponent } from '../../components/more/accordion/accordion.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [SlideShowComponent, SlideShowGalleryComponent],
+  imports: [CommonModule, SlideShowComponent, SlideShowGalleryComponent, AccordionComponent],
   templateUrl: './components.component.html',
   styleUrl: './components.component.scss'
 })
